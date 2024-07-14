75290685087d7020effed5aa1a1093cf04b4b3b4
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-07-14 00:09:47 -0600

    Add content-editor module and it's basic routing

---
+++ src/app/content-editor/content-editor.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditorComponent } from './editor/editor.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MetaComponent } from './meta/meta.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' }, // redirect to root 
+  { path: 'new', title: 'New Post', component: MetaComponent },
+  { path: ':post-id', title: 'Editor', component: EditorComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class ContentEditorModule { }
