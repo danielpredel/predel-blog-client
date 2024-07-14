3f9d1598e4f6fe269965c4fa5ab55bb22365078b
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-07-14 00:28:13 -0600

    Add speed dial component to editor module and create basic functionality

---
+++ src/app/content-editor/speed-dial/speed-dial.component.ts
@@ -0,0 +1,24 @@
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-speed-dial',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './speed-dial.component.html',
+  styleUrl: './speed-dial.component.css'
+})
+export class SpeedDialComponent {
+  @Input() open: boolean = false;
+  @Output() statusChage = new EventEmitter<boolean>();
+  @Output() selection = new EventEmitter<string>();
+
+  toggleMenu() {
+    if (this.open) {
+      this.statusChage.emit(false);
+    }
+    else {
+      this.statusChage.emit(true);
+    }
+  }
+}
