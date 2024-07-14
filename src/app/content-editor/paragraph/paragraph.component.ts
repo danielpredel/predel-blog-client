97cb18e8a828353e2ba3cec7aeac5ab613471d09
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-07-14 00:30:13 -0600

    Add paragraph component to editor module and create basic functionality for editable elements

---
+++ src/app/content-editor/paragraph/paragraph.component.ts
@@ -0,0 +1,98 @@
+import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Editable } from '../editable';
+import { NgIf } from '@angular/common';
+import { SpeedDialComponent } from '../speed-dial/speed-dial.component';
+
+@Component({
+  selector: 'app-paragraph',
+  standalone: true,
+  imports: [NgIf, SpeedDialComponent],
+  templateUrl: './paragraph.component.html',
+  styleUrl: './paragraph.component.css'
+})
+export class ParagraphComponent implements AfterViewInit, Editable {
+  @Output() enterPressed = new EventEmitter<ParagraphComponent>();
+  @Output() backspacePressed = new EventEmitter<ParagraphComponent>();
+  @ViewChild('editableParagraph', { read: ElementRef }) editableElement: ElementRef | undefined;
+  @ViewChild('editableParagraph', { read: ElementRef }) cursorElement: ElementRef<HTMLParagraphElement> | undefined;
+  showSpeedDial: boolean = true;
+  showSpeedDialOptions: boolean = false;
+
+  ngAfterViewInit(): void {
+    this.focus();
+  }
+
+  onKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      this.showSpeedDial = false;
+      this.enterPressed.emit();
+    }
+    else if (event.key === 'Backspace') {
+      if (this.editableElement) {
+        const text = this.editableElement.nativeElement.textContent.trim();
+        if (!text) {
+          this.backspacePressed.emit();
+        }
+        if (text.length == 1) {
+          this.showSpeedDial = true;
+        }
+      }
+    }
+  }
+
+  onKeyUp(event: KeyboardEvent): void {
+    if (this.editableElement) {
+      const text = this.editableElement.nativeElement.textContent.trim();
+      // console.log(text.length)
+      if (text.length > 0 && this.showSpeedDial) {
+        this.showSpeedDial = false;
+      }
+      if (text.length == 1) {
+        if (text == '*' && event.code == 'Space') {
+          console.log('UL')
+        }
+        if (text == '+' && event.code == 'Space') {
+          console.log('OL')
+        }
+      }
+    }
+  }
+
+  focus(): void {
+    if (this.editableElement) {
+      this.editableElement.nativeElement.focus();
+    }
+  }
+
+  placeCursorAtEnd(): void {
+    if (this.cursorElement) {
+      const element = this.cursorElement.nativeElement;
+      const range = document.createRange();
+      const selection = window.getSelection();
+      range.selectNodeContents(element);
+      range.collapse(false);
+      if (selection) {
+        selection.removeAllRanges();
+        selection.addRange(range);
+      }
+    }
+  }
+
+  onSpeedStatusChange(open: boolean) {
+    this.showSpeedDialOptions = open;
+  }
+
+  onFocus() {
+    if (this.editableElement) {
+      const text = this.editableElement.nativeElement.textContent.trim();
+      if (text.length == 0) {
+        this.showSpeedDial = true;
+      }
+      else {
+        this.showSpeedDial = false;
+      }
+    }
+    this.showSpeedDialOptions = false;
+  }
+}
