04f3bd77fb58a9467a8977a36b89bb405170e65f
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-07-14 00:07:19 -0600

    Add navbar and basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './home/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomepageComponent },
+    { path: 'editor', loadChildren: () => import('./content-editor/content-editor.module').then(m => m.ContentEditorModule) }
+];
