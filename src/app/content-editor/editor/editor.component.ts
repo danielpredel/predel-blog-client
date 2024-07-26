69772f10f6c5b1b872c757ea9cb07d4e514a6bb8
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-07-25 21:02:00 -0600

    Change in selection strategy, move tooltip's logic to it's own component

--- src/app/content-editor/editor/editor.component.ts
+++ src/app/content-editor/editor/editor.component.ts
@@ -1,33 +1,27 @@
-import { Component, ComponentRef, ElementRef, HostListener, ViewChild, ViewContainerRef } from '@angular/core';
-import { ParagraphComponent } from '../paragraph/paragraph.component';
+import { Component, ComponentRef, ElementRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { ListComponent } from '../list/list.component';
 import { TextComponent } from "../text/text.component";
-import { TooltipComponent } from "../tooltip/tooltip.component";
 import { NgClass, NgIf, NgStyle } from '@angular/common';
 import { IdService } from '../../id.service';
+import { TooltipComponent } from "../tooltip/tooltip.component";
 
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [TextComponent, TooltipComponent, NgIf, NgClass, NgStyle],
+  imports: [NgIf, NgClass, NgStyle, TooltipComponent],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.css'
 })
 export class EditorComponent {
-  // Tooltip variables
-  tooltipVisible: boolean = false;
-  tooltipLeft: number = 0;
-  tooltipTop: number = 0;
-  tooltipLink = { disabled: false, selected: false };
-  tooltipTitle = { disabled: false, selected: false };
-  tooltipSubtitle = { disabled: false, selected: false };
-  tooltipTarget: string = '';
-
   // Element's Varibles
   @ViewChild('container', { read: ViewContainerRef, static: true }) container!: ViewContainerRef;
+  @ViewChild(TooltipComponent) tooltip!: TooltipComponent;
+
   private components: ComponentRef<any>[] = [];
   private componentsIds: Array<string> = [];
-  commonAncestorElement: Element | null | undefined;
+
+  selectionRange: Range | undefined;
+  selectionTargetId: string = '';
 
   constructor(private idService: IdService) { }
 
@@ -44,114 +38,107 @@ export class EditorComponent {
   onMouseUp() {
     const selection = window.getSelection();
     if (selection) {
-      if (selection.toString().length == 0) {
-        this.tooltipVisible = false;
-      }
-      else {
-        const commonAncestor = selection?.getRangeAt(0).commonAncestorContainer as Element;
-        this.commonAncestorElement = commonAncestor?.nodeType !== 1 ? commonAncestor?.parentElement : commonAncestor;
-        const ancestorId = this.commonAncestorElement?.id;
-        if (ancestorId) {
-          if (ancestorId.length >= 14) {
-            this.tooltipTarget = ancestorId.substring(0, 14);
-
-            this.restoreTooltip();
-            switch (true) {
-              case ancestorId?.includes('txt'):
-                switch (true) {
-                  case ancestorId?.includes('Paragraph'):
-                    break;
-                  case ancestorId?.includes('Title'):
-                    this.tooltipLink.disabled = true;
-                    this.tooltipTitle.selected = true;
-                    break;
-                  case ancestorId?.includes('Subtitle'):
-                    this.tooltipLink.disabled = true;
-                    this.tooltipSubtitle.selected = true;
-                    break;
-                }
-                this.showTooltip();
-                break;
-              case ancestorId?.includes('lsi'):
-                this.tooltipTitle.disabled = true;
-                this.tooltipSubtitle.disabled = true;
-                this.showTooltip();
-                break;
-            }
-          }
-        }
-      }
-    }
-    else {
-      this.tooltipVisible = false;
+      this.handleSelection(selection);
     }
   }
 
   onMouseDown() {
-    window.getSelection()?.removeAllRanges();
+    this.tooltip.hide();
+    this.hideSelection();
   }
 
-  onTooltipSelection(type: string) {
-    let targetIndex = this.componentsIds.indexOf(this.tooltipTarget)
-    if (targetIndex > -1) {
-      if (this.components[targetIndex].location.nativeElement.contains(this.commonAncestorElement)) {
-        switch (type) {
-          case 'link':
+  onTooltipSelection(operation: any) {
+    console.log(operation);
+    // console.log(this.components[targetIndex].location.nativeElement.contains(this.commonAncestorElement))
+  }
+
+  // Functions
+  handleSelection(selection: Selection) {
+    if (selection.toString().length > 0) {
+      this.selectionRange = selection?.getRangeAt(0);
+      const ancestorId = this.getCommonAncestorsId();
+
+      if (ancestorId && ancestorId.length >= 14) {
+        this.selectionTargetId = ancestorId.substring(0, 14);
+        switch (true) {
+          case ancestorId?.includes('txt'):
+            switch (true) {
+              case ancestorId?.includes('Paragraph'):
+                if (ancestorId?.includes('anchor')) {
+                  this.tooltip.setConfig('onLinkedParagraph');
+                }
+                else {
+                  if (this.selectionRange.startContainer === this.selectionRange.endContainer) {
+                    this.tooltip.restoreConfig();
+                  }
+                  else {
+                    this.tooltip.setConfig('onLinkedParagraph');
+                  }
+                }
+                break;
+              case ancestorId?.includes('Title'):
+                this.tooltip.setConfig('onTitle');
+                break;
+              case ancestorId?.includes('Subtitle'):
+                this.tooltip.setConfig('onSubtitle');
+                break;
+            }
+            this.tooltip.setClientRect(this.selectionRange.getBoundingClientRect());
+            this.tooltip.placeForOptions();
             break;
-          case 'title':
-            if (!this.tooltipTitle.disabled) {
-              if (this.tooltipTitle.selected) {
-                this.components[targetIndex].instance.toParagraph();
+          case ancestorId?.includes('lsi'):
+            if (ancestorId?.includes('anchor')) {
+              this.tooltip.setConfig('onLinkedListItem');
+            }
+            else {
+              if (this.selectionRange.startContainer === this.selectionRange.endContainer) {
+                this.tooltip.setConfig('onListItem');
               }
               else {
-                this.components[targetIndex].instance.toTitle();
+                this.tooltip.setConfig('onLinkedListItem');
               }
             }
-            break;
-          case 'subtitle':
-            if (!this.tooltipSubtitle.disabled) {
-              if (this.tooltipSubtitle.selected) {
-                this.components[targetIndex].instance.toParagraph();
-              }
-              else {
-                this.components[targetIndex].instance.toSubtitle();
-              }
-            }
+            this.tooltip.setClientRect(this.selectionRange.getBoundingClientRect());
+            this.tooltip.placeForOptions();
             break;
         }
       }
     }
-    this.tooltipVisible = false;
   }
 
-  // Functions
-  restoreTooltip() {
-    this.tooltipLink = { disabled: false, selected: false };
-    this.tooltipTitle = { disabled: false, selected: false };
-    this.tooltipSubtitle = { disabled: false, selected: false };
+  hideSelection() {
+    window.getSelection()?.removeAllRanges();
   }
 
-  showTooltip() {
-    const clientRect = window.getSelection()?.getRangeAt(0).getBoundingClientRect();
-    if (clientRect) {
-      this.tooltipLeft = clientRect.right - (clientRect.right - clientRect.left) / 2 - 51;
-      this.tooltipTop = clientRect.top - 45;
-      this.tooltipVisible = true;
+  restoreSelection() {
+    console.log('a')
+    const selection = window.getSelection();
+    selection?.removeAllRanges();
+    console.log(this.selectionRange)
+    if (this.selectionRange) {
+      selection?.addRange(this.selectionRange);
     }
-    else {
-      this.tooltipVisible = false;
-    }
+  }
+
+  getCommonAncestorsId() {
+    const commonAncestor = this.selectionRange?.commonAncestorContainer as Element;
+    const commonAncestorElement = commonAncestor?.nodeType !== 1 ? commonAncestor?.parentElement : commonAncestor;
+    return commonAncestorElement?.id;
   }
 
   renderNewPost() {
     this.addTextComponent(0);
-    this.components[0].instance.toTitle();
+    // this.components[0].instance.toTitle();
   }
 
-  renderEditPost() {}
+  renderEditPost() {
+    // console.log();
+    // 
+  }
 
   addTextComponent(index: number, data: any = null) {
     const componentRef = this.container.createComponent(TextComponent, { index });
+
     let id = this.idService.getId();
     let ids = Array();
     ids.push(`txt-${id}`);
@@ -169,12 +156,14 @@ export class EditorComponent {
 
     if (index == 0) {
       this.components.push(componentRef);
+      // console.log(componentRef.componentType === TextComponent);
       this.componentsIds.push(ids[0]);
     }
     else {
       this.components.splice(index, 0, componentRef);
       this.componentsIds.splice(index, 0, ids[0]);
 
+      // Suscribe only if it's not the first Component in the ViewContainerRef 
       componentRef.instance.deleteComponent.subscribe(() => {
         this.removeTextComponent(index);
       });
@@ -183,65 +172,66 @@ export class EditorComponent {
     componentRef.instance.addComponent.subscribe(() => {
       this.addTextComponent(index + 1);
     });
-    // if (index > 0) {
-    //   componentRef.instance.deleteComponent.subscribe(() => {
-    //     this.removeTextComponent(index);
-    //   });
-    // }
     componentRef.instance.changeComponent.subscribe((componentType) => {
       this.changeTextComponent(index, componentType);
     });
+
     setTimeout(() => {
-      if (index > 0) {
-        this.components[index - 1].instance.blur();
-      }
       componentRef.instance.focus();
     }, 0);
   }
 
-  addComponent(type: string) {
-    const componentRef = this.container.createComponent(ParagraphComponent);
-    this.components.push(componentRef);
+  // addComponent(type: string) {
+  //   const componentRef = this.container.createComponent(ParagraphComponent);
+  //   this.components.push(componentRef);
 
-    componentRef.instance.newComponent.subscribe(() => {
-      this.addComponentAtIndex(1, 'P');
-    });
-    // componentRef.instance.deleteMe.subscribe(() => {
-    //   this.removeComponentAtIndex(0);
-    // });
-    componentRef.instance.changeElement.subscribe((componentType) => {
-      this.changeComponentAtIndex(0, componentType);
-    });
-    setTimeout(() => {
-      this.components[0].instance.blur();
-      componentRef.instance.focus()
-    }, 0);
-  }
+  //   componentRef.instance.newComponent.subscribe(() => {
+  //     this.addComponentAtIndex(1, 'P');
+  //   });
+  //   // componentRef.instance.deleteMe.subscribe(() => {
+  //   //   this.removeComponentAtIndex(0);
+  //   // });
+  //   componentRef.instance.changeElement.subscribe((componentType) => {
+  //     this.changeComponentAtIndex(0, componentType);
+  //   });
+  //   setTimeout(() => {
+  //     this.components[0].instance.blur();
+  //     componentRef.instance.focus()
+  //   }, 0);
+  // }
 
-  addComponentAtIndex(index: number, type: string) {
-    const componentRef = this.container.createComponent(ParagraphComponent, { index });
-    this.components.splice(index, 0, componentRef);
-    const componentIndex = this.components.indexOf(componentRef);
-    componentRef.instance.newComponent.subscribe(() => {
-      this.addComponentAtIndex(componentIndex + 1, 'P');
-    });
-    if (componentIndex > 0) {
-      componentRef.instance.deleteMe.subscribe(() => {
-        this.removeComponentAtIndex(componentIndex);
-      });
-    }
-    componentRef.instance.changeElement.subscribe((componentType) => {
-      this.changeComponentAtIndex(componentIndex, componentType)
-    });
+  // addComponentAtIndex(index: number, type: string) {
+  //   const componentRef = this.container.createComponent(ParagraphComponent, { index });
+  //   this.components.splice(index, 0, componentRef);
+  //   const componentIndex = this.components.indexOf(componentRef);
+  //   componentRef.instance.newComponent.subscribe(() => {
+  //     this.addComponentAtIndex(componentIndex + 1, 'P');
+  //   });
+  //   if (componentIndex > 0) {
+  //     componentRef.instance.deleteMe.subscribe(() => {
+  //       this.removeComponentAtIndex(componentIndex);
+  //     });
+  //   }
+  //   componentRef.instance.changeElement.subscribe((componentType) => {
+  //     this.changeComponentAtIndex(componentIndex, componentType)
+  //   });
 
-    setTimeout(() => {
-      this.components[index - 1].instance.blur();
-      componentRef.instance.focus()
-    }, 0);
-  }
+  //   setTimeout(() => {
+  //     this.components[index - 1].instance.blur();
+  //     componentRef.instance.focus()
+  //   }, 0);
+  // }
 
   removeTextComponent(index: number) {
-
+    const component = this.components.splice(index, 1)[0];
+    component.instance.deleteMe.unsubscribe();
+    component.instance.newComponent.unsubscribe();
+    this.container.remove(index);
+    setTimeout(() => {
+      this.components[index - 1].instance.focus();
+      this.components[index - 1].instance.placeCursorAtEnd();
+      // Add the tada after the cursor, in case there's any
+    }, 0);
   }
 
   removeComponentAtIndex(index: number) {
@@ -265,7 +255,8 @@ export class EditorComponent {
   }
 
   changeTextComponent(index: number, newComponentType: string) {
-
+    this.container.detach(index);
+    this.addListAtIndex(index, newComponentType);
   }
 
   addListAtIndex(index: number, type: string) {
