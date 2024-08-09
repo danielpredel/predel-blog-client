b341bb85ad5abbdbc411b64fb25d17b63b632c48
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-08-09 00:07:27 -0600

    Add src to image component for dynamic rendering

--- src/app/content-editor/image/image.component.ts
+++ src/app/content-editor/image/image.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './image.component.css'
 })
 export class ImageComponent {
-
+  // src: string = 'https://cdn.environment.sa.gov.au/img/eyJidWNrZXQiOiJlbnZzYS1idWNrZXQiLCJrZXkiOiJsYW5kc2NhcGUvaW1hZ2VzL3N0YXRld2lkZS9sYnNhLWhlcm9fMjAyMy0wOC0wOC0wMTU5MjRfdWpzaC5qcGciLCJlZGl0cyI6eyJ3ZWJwIjp7InF1YWxpdHkiOjgyfSwicmVzaXplIjp7IndpZHRoIjoxMjAwLCJoZWlnaHQiOjQ5NSwiZml0IjoiY292ZXIifX19';
+  src: string = '';
+  alt: string = '';
+  caption: string = '';
 }
