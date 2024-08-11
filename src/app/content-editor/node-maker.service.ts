2d8721165884afee66f32e99e6b001f9a8555d31
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-08-11 03:00:51 -0600

    Add funtions to create bold, italic, strikethrough and link nodes

--- src/app/content-editor/node-maker.service.ts
+++ src/app/content-editor/node-maker.service.ts
@@ -6,4 +6,38 @@ import { Injectable } from '@angular/core';
 export class NodeMakerService {
 
   constructor() { }
+
+  createTextNode(text: string) {
+    const node = document.createTextNode(text);
+    return node;
+  }
+
+  createLinkNode(text: string, url: string, id: string) {
+    const node = document.createElement('a');
+    node.href = url;
+    node.id = id;
+    node.textContent = text;
+    return node;
+  }
+
+  createBoldNode(text: string, id: string) {
+    const node = document.createElement('b');
+    node.id = id;
+    node.textContent = text;
+    return node;
+  }
+
+  createItalicNode(text: string, id: string) {
+    const node = document.createElement('i');
+    node.id = id;
+    node.textContent = text;
+    return node;
+  }
+
+  createStrikeNode(text: string, id: string) {
+    const node = document.createElement('s');
+    node.id = id;
+    node.textContent = text;
+    return node;
+  }
 }
