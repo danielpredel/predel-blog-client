34c6f6500111639a42bbe9a50436b60038aa18e4
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-08-24 18:24:02 -0600

    Add Modal with Reactive Form for Image URL and Alt. Add change in data for selection event emitter

--- src/app/content-editor/speed-dial/speed-dial.component.ts
+++ src/app/content-editor/speed-dial/speed-dial.component.ts
@@ -1,16 +1,22 @@
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-speed-dial',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgClass, ReactiveFormsModule],
   templateUrl: './speed-dial.component.html',
   styleUrl: './speed-dial.component.css'
 })
 export class SpeedDialComponent {
   open: boolean = false;
-  @Output() selection = new EventEmitter<string>();
+  @Output() selection = new EventEmitter<any>();
+
+  imageForm = new FormGroup({
+    src: new FormControl('', [Validators.required, this.urlImageValidator]),
+    alt: new FormControl('')
+  });
 
   openMenu() {
     this.open = true;
@@ -22,6 +28,22 @@ export class SpeedDialComponent {
 
   setSection(type: string) {
     this.closeMenu();
-    this.selection.emit(type);
+    this.selection.emit({ type });
+  }
+
+  // Form validation
+  urlImageValidator(control: FormControl): { [key: string]: any } | null {
+    const pattern = /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|png|gif|bmp|svg|webp)$/;
+    if (!pattern.test(control.value)) {
+      return { urlError: 'URL Error' };
+    }
+    return null;
+  }
+
+  onSubmit() {
+    this.closeMenu();
+    if (this.imageForm.valid) {
+      this.selection.emit({ type: 'IMAGE', src: this.imageForm.get('src')?.value, alt: this.imageForm.get('alt')?.value });
+    }
   }
 }
