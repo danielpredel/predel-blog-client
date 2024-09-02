23b418002a7ddbaf2396fb2ed84a0dc0953d181c
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-02 00:30:16 -0600

    Add user module to routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { HomepageComponent } from './home/homepage/homepage.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent },
-    { path: 'editor', loadChildren: () => import('./content-editor/content-editor.module').then(m => m.ContentEditorModule) }
+    { path: 'editor', loadChildren: () => import('./content-editor/content-editor.module').then(m => m.ContentEditorModule) },
+    { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }
 ];
