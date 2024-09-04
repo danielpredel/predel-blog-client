3f448fff70cd53bb09bbb076afc5b67d3adf7f37
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-04 10:57:55 -0600

    Create user service. Add functions to create a new user and create a new post

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url = environment.baseUrl + '/users'
+
+  constructor(private http: HttpClient) { }
+
+  createUser(name: string, lastname: string, image: string,
+    email: string, password: string, confirmPassword: string) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+    const body = new HttpParams()
+      .set('name', name)
+      .set('lastname', lastname)
+      .set('image', image)
+      .set('email', email)
+      .set('password', password)
+      .set('confirmPassword', confirmPassword);
+    return this.http.post<any>(this.url, body, { headers });
+  }
+
+  createPost(title: string, image: string) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+    const body = new HttpParams()
+      .set('title', title)
+      .set('image', image);
+    return this.http.post<any>(this.url + '/posts', body, { headers });
+  }
+}
