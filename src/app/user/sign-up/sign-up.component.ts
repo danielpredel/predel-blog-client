2f87ef510e1df0f8cf2421146949b3d5ff1e40e4
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-03 18:47:15 -0600

    Remove local validators and add validators from service

--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
 import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { matchPasswordValidator } from '../match.password.validator';
+import { ValidatorsService } from '../../shared/services/validators.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,24 +12,33 @@ import { matchPasswordValidator } from '../match.password.validator';
 })
 export class SignUpComponent {
 
+  constructor(private validatorService: ValidatorsService) { }
+
   form = new FormGroup({
     name: new FormControl('', {
       validators: [Validators.required, Validators.minLength(2),
-      Validators.maxLength(20), this.whitespaceValidator],
+      Validators.maxLength(20), this.validatorService.noWhitespace],
       updateOn: 'change'
     }),
     lastname: new FormControl('', {
       validators: [Validators.required, Validators.minLength(2),
-      Validators.maxLength(30), this.whitespaceValidator],
+      Validators.maxLength(30), this.validatorService.noWhitespace],
       updateOn: 'change'
     }),
+    image: new FormControl('', {
+      validators: [Validators.required],
+      asyncValidators: [this.validatorService.imageUrl()],
+      updateOn: 'blur'
+    }),
+    // check unique email in API
     email: new FormControl('', {
-      validators: [Validators.required, Validators.email, this.emailDomainValidator],
+      validators: [Validators.required, Validators.email, this.validatorService.emailDomain],
       updateOn: 'change'
     }),
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(8),
-      this.minusValidator, this.mayusValidator, this.digitsValidator, this.symbolsValidator
+      this.validatorService.minus, this.validatorService.mayus,
+      this.validatorService.digits, this.validatorService.symbols
       ],
       updateOn: 'change'
     }),
@@ -37,86 +46,7 @@ export class SignUpComponent {
       validators: [Validators.required],
       updateOn: 'change'
     }),
-    image: new FormControl('', {
-      validators: [Validators.required, this.urlImageValidator],
-      updateOn: 'change'
-    }),
-  }, { validators: matchPasswordValidator, updateOn: 'submit' });
-
-  // Form validation
-  urlImageValidator(control: FormControl): { [key: string]: any } | null {
-    const pattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)\.(?:jpg|jpeg|png|gif|bmp|svg|webp)$/;
-    if (!pattern.test(control.value)) {
-      return { urlError: 'URL Error' };
-    }
-    return null;
-  }
-
-  emailDomainValidator(control: FormControl): { [key: string]: any } | null {
-    if (control.value) {
-      const email = control.value;
-      const domain = email.split('@')[1]?.toLowerCase();
-      const allowedDomains = ['gmail.com', 'outlook.com'];
-      if (!allowedDomains.includes(domain)) {
-        return { emailDomainError: 'URL Error' };
-      }
-    }
-    return null;
-  }
-
-  whitespaceValidator(control: FormControl): { [key: string]: any } | null {
-    if (control.value) {
-      const name = control.value as string;
-      if ((name || '').trim().length === 0) {
-        return { whitespaceError: true };
-      }
-    }
-    return null;
-  }
-
-  minusValidator(control: FormControl): { [key: string]: any } | null {
-    if (control.value) {
-      const password = control.value;
-      const pattern = /.*[a-z].*[a-z]/;
-      if (!pattern.test(password)) {
-        return { minusError: true }
-      }
-    }
-    return null;
-  }
-
-  mayusValidator(control: FormControl): { [key: string]: any } | null {
-    if (control.value) {
-      const password = control.value;
-      const pattern = /.*[A-Z].*[A-Z]/;
-      if (!pattern.test(password)) {
-        return { mayusError: true }
-      }
-    }
-    return null;
-  }
-
-  digitsValidator(control: FormControl): { [key: string]: any } | null {
-    if (control.value) {
-      const password = control.value;
-      const pattern = /.*\d.*\d/;
-      if (!pattern.test(password)) {
-        return { digitsError: true }
-      }
-    }
-    return null;
-  }
-
-  symbolsValidator(control: FormControl): { [key: string]: any } | null {
-    if (control.value) {
-      const password = control.value;
-      const pattern = /.*[!@#$%^&*(),.?":{}|<>].*[!@#$%^&*(),.?":{}|<>]/;
-      if (!pattern.test(password)) {
-        return { symbolsError: true }
-      }
-    }
-    return null;
-  }
+  }, { validators: this.validatorService.matchPasswordValidator, updateOn: 'submit' });
 
   onSubmit() {
     if (this.form.valid) {
