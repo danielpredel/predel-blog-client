33f0cb69a3920725efe539cfaba17e6fce58af30
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-05 08:47:55 -0600

    Show navbar items depnding on the existance of a session or not. Change profile image display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from './shared/services/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'predel_blog_client';
+  constructor(private authService: AuthService) { }
+
+  isLogged(){
+    return this.authService.isLogged();
+  }
+
+  getProfileImageUrl(){
+    return this.authService.getProfileImageUrl();
+  }
+
+  logout(){
+    this.authService.clearSession();
+  }
 }
