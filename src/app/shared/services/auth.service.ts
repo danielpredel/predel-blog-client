c1744a0c6c7e673cd539a239a5d7e97c082ee3c3
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-05 08:42:45 -0600

    Add session functions in auth service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -6,4 +6,58 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  setSession(token: string, profileImageUrl: string, verified: boolean) {
+    this.setToken(token);
+    this.setProfileImageUrl(profileImageUrl);
+    this.setVerified(verified);
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  setProfileImageUrl(profileImageUrl: string) {
+    localStorage.setItem('profileImageUrl', profileImageUrl);
+  }
+
+  setVerified(verified: boolean) {
+    localStorage.setItem('verified', verified ? 'true' : 'false');
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  getProfileImageUrl() {
+    return localStorage.getItem('profileImageUrl');
+  }
+
+  getVerified() {
+    let verified = localStorage.getItem('verified');
+    if (verified) {
+      return verified === 'true' ? true : false;
+    }
+    return null;
+  }
+
+  removeToken() {
+    localStorage.removeItem('token');
+  }
+
+  removeProfileImageUrl() {
+    localStorage.removeItem('profileImageUrl');
+  }
+
+  removeVerified() {
+    localStorage.removeItem('verified');
+  }
+
+  clearSession() {
+    localStorage.clear();
+  }
+
+  isLogged(){
+    return this.getToken() ? true : false;
+  }
 }
