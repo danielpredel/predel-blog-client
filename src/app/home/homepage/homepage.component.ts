ddf670f7748ba48289ae84963503be3edde06f6b
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-08 14:42:36 -0600

    Redirect to post

--- src/app/home/homepage/homepage.component.ts
+++ src/app/home/homepage/homepage.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BlogEntryComponent } from "../blog-entry/blog-entry.component";
 import { CommonModule, NgClass, NgFor } from '@angular/common';
 import { PostService } from '../../shared/services/post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -14,7 +15,7 @@ export class HomepageComponent {
   loading: boolean = true;
   error: boolean = false;
 
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService, private router: Router) { }
   posts: Array<{ _id: string, title: string, image: string, author: string, authorImage: string }> = [];
 
   ngOnInit() {
@@ -29,11 +30,7 @@ export class HomepageComponent {
     });
   }
 
-  onClick(index: number) {
-    alert(index);
-  }
-
-  toPost(id: string){
-    alert(id)
+  toPost(id: string) {
+    this.router.navigate(['/post', id]);
   }
 }
