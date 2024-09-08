ba4b2ffdda26b233ac04994693da5a141222fb8e
danielpredel <danielpreciadodelgadillo@gmail.com>
2024-09-08 15:49:16 -0600

    Add statics list item functionality

--- src/app/home/list-item/list-item.component.ts
+++ src/app/home/list-item/list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NodeMakerService } from '../../shared/services/node-maker.service';
 
 @Component({
   selector: 'app-list-item',
@@ -9,4 +10,43 @@ import { Component } from '@angular/core';
 })
 export class ListItemComponent {
 
+  // DOM Manipulation Variables
+  @ViewChild('listItem', { read: ElementRef }) listItem: ElementRef<HTMLLIElement> | undefined;
+
+  constructor(private nodeMakerService: NodeMakerService) { }
+
+  // Should receive an Array
+  setData(data: Array<any>) {
+    if (data.length > 0) {
+      this.setDataAtEnd(data);
+    }
+  }
+
+  // Should receive an Array
+  setDataAtEnd(data: Array<any>) {
+    data.forEach((element: { type: string; text: string; url: string }) => {
+      let node = null;
+      switch (element.type) {
+        case 'text':
+          node = this.nodeMakerService.createTextNode(element.text);
+          break;
+        case 'bold':
+          node = this.nodeMakerService.createBoldNode(element.text);
+          break;
+        case 'italic':
+          node = this.nodeMakerService.createItalicNode(element.text);
+          break;
+        case 'strike':
+          node = this.nodeMakerService.createStrikeNode(element.text);
+          break;
+        case 'link':
+          node = this.nodeMakerService.createLinkNode(element.text, element.url);
+          break;
+      }
+      if (node) {
+        this.listItem?.nativeElement.appendChild(node);
+      }
+    });
+    this.listItem?.nativeElement.normalize();
+  }
 }
